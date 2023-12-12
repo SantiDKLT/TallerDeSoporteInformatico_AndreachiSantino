89c61a6106a1df41aafd973ef4e9349006f2d745
SantiDKLT <101186059+SantiDKLT@users.noreply.github.com>
2023-12-12 14:29:39 -0300

    Update script.js

--- TallerDeSopInformatico/ValidacionesFormularioConJquery/script.js
+++ TallerDeSopInformatico/ValidacionesFormularioConJquery/script.js
@@ -1,29 +1,33 @@
-function validarFormulario() {
+  function validarFormulario() {
     // Obtenemos los valores de los campos
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
-    var mensaje = document.getElementById("mensaje").value;
+    var nombre = $("#nombre").val();
+    var email = $("#email").val();
+    var mensaje = $("#mensaje").val();
 
     // Verificación simple del correo electrónico
-    if (!validarEmail(email)){
-      alert("Por favor, ingrese un correo electrónico válido.").css("background-color","red");
+    if (!validarEmail(email)) {
+      mostrarError("Por favor, ingrese un correo electrónico válido.");
       return;
     }
 
     // Verificación de la longitud del mensaje
     if (mensaje.length < 10) {
-      alert("El mensaje debe tener al menos 10 caracteres.").css("background-color","red");
+      mostrarError("El mensaje debe tener al menos 10 caracteres.");
       return;
     }
 
-    // Puedes agregar más verificaciones aquí para otros campos
-
     // Si todas las verificaciones pasan, puedes enviar el formulario
-    document.getElementById("myForm").submit();
+    $("#myForm").submit();
   }
 
   function validarEmail(email) {
     // Verificación simple de correo electrónico
     var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
+  }
+
+  function mostrarError(mensaje) {
+    // Muestra un mensaje de error
+    $("#errorMensaje").text(mensaje);
+    $("#errorAlert").show();
   }
