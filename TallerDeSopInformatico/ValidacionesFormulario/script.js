0e3c21ff314b0ef17cee4767e48858e567963f86
SantiDKLT <101186059+SantiDKLT@users.noreply.github.com>
2023-12-12 14:19:23 -0300

    Add files via upload

---
+++ TallerDeSopInformatico/ValidacionesFormulario/script.js
@@ -0,0 +1,29 @@
+function validarFormulario() {
+    // Obtenemos los valores de los campos
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+
+    // Verificación simple del correo electrónico
+    if (!validarEmail(email)) {
+      alert("Por favor, ingrese un correo electrónico válido.");
+      return;
+    }
+
+    // Verificación de la longitud del mensaje
+    if (mensaje.length < 10) {
+      alert("El mensaje debe tener al menos 10 caracteres.");
+      return;
+    }
+
+    // Puedes agregar más verificaciones aquí para otros campos
+
+    // Si todas las verificaciones pasan, puedes enviar el formulario
+    document.getElementById("myForm").submit();
+  }
+
+  function validarEmail(email) {
+    // Verificación simple de correo electrónico
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
